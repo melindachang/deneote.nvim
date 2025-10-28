e61a0efd7908449862be50d090d7aed0334e6226
Melinda Chang <melindachang.hy@gmail.com>
2025-10-28 01:34:27 -0500

    feat(keymap): add default keymap for create command

--- lua/deneote/init.lua
+++ lua/deneote/init.lua
@@ -1,13 +1,14 @@
 local Config = require('deneote.config')
 local Cmd = require('deneote.cmd')
-local Utils = require('deneote.utils')
+local Keymap = require('deneote.keymap')
 
 local M = {}
 
----@param opts DeneoteConfig
+---@param opts DeneoteUserConfig
 function M.setup(opts)
   Config.setup(opts)
   Cmd.setup()
+  Keymap.setup()
 end
 
 return M
