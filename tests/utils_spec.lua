99ee6cedfcb957b2d2a19bcad1d6f71ac0886a84
Melinda Chang <melindachang.hy@gmail.com>
2025-10-31 02:11:04 -0500

    test: update sluggify_tag to sluggify_keyword

--- tests/utils_spec.lua
+++ tests/utils_spec.lua
@@ -1,75 +1,85 @@
-local _ = require('plenary.busted')
+---@module 'plenary.busted'
 
 local eq, match = assert.are.same, assert.are.match
-local Utils = require('deneote.utils')
+local utils = require('deneote.utils')
+local fs = require('deneote.utils.fs')
 
-describe('Utils.map()', function()
+describe('utils.map()', function()
   it('procs function on table values', function()
     eq(
       { 2, 4, 6 },
-      Utils.map({ 1, 2, 3 }, function(v)
+      utils.map({ 1, 2, 3 }, function(v)
         return v * 2
       end)
     )
   end)
 end)
 
-describe('Utils.make_timestamp()', function()
+describe('fs.make_timestamp()', function()
   it('returns string in expected format', function()
-    local ts = Utils.make_timestamp()
+    local ts = fs.make_timestamp()
     match('^%d%d%d%d%d%d%d%dT%d%d%d%d%d%d$', ts)
   end)
 end)
 
-describe('Utils.slug_sanitize()', function()
+describe('fs.slug_sanitize()', function()
   it('removes illegal filename characters', function()
-    eq('file', Utils.slug_sanitize('|f/i*l:e?<>'))
+    eq('file', fs.slug_sanitize('|f/i*l:e?<>'))
   end)
 
   it('replaces non-ASCII characters with spaces', function()
     eq(
       'There are no-ASCII     characters     here     ',
-      Utils.slug_sanitize('There are no-ASCII ： characters ｜ here 😀')
+      fs.slug_sanitize('There are no-ASCII ： characters ｜ here 😀')
     )
   end)
 
   it('removes symbols from string', function()
-    eq('This-is-test', Utils.slug_sanitize('This-is-!@#test'))
+    eq('This-is-test', fs.slug_sanitize('This-is-!@#test'))
   end)
 end)
 
-describe('Utils.slug_hyphenate()', function()
+describe('fs.slug_hyphenate()', function()
   it('hyphenates string', function()
-    eq('This-is-a-test', Utils.slug_hyphenate('__  This is   a    test  __  '))
+    eq('This-is-a-test', fs.slug_hyphenate('__  This is   a    test  __  '))
 
-    eq('!~!!$%^-This-iS-a-tEsT-++-??', Utils.slug_hyphenate(' ___ !~!!$%^ This iS a tEsT ++ ?? '))
+    eq(
+      '!~!!$%^-This-iS-a-tEsT-++-??',
+      fs.slug_hyphenate(' ___ !~!!$%^ This iS a tEsT ++ ?? ')
+    )
   end)
   it('replaces multiple hyphens with single hyphen', function()
-    eq('foo-bar', Utils.slug_hyphenate('foo---bar'))
-    eq('foo-bar', Utils.slug_hyphenate('foo__-bar'))
+    eq('foo-bar', fs.slug_hyphenate('foo---bar'))
+    eq('foo-bar', fs.slug_hyphenate('foo__-bar'))
   end)
   it('trims hyphens from ends of string', function()
-    eq('foo', Utils.slug_hyphenate('--foo-'))
-    eq('foo', Utils.slug_hyphenate('- __-foo-__  '))
+    eq('foo', fs.slug_hyphenate('--foo-'))
+    eq('foo', fs.slug_hyphenate('- __-foo-__  '))
   end)
 end)
 
-describe('Utils.sluggify_title()', function()
-  it('downcases, hyphenates, de-punctuates, and removes spaces from string', function()
-    eq('this-is-a-test', Utils.sluggify_title(' ___ !~!!$%^ This iS a tEsT ++ ?? '))
-  end)
+describe('fs.sluggify_title()', function()
+  it(
+    'downcases, hyphenates, de-punctuates, and removes spaces from string',
+    function()
+      eq(
+        'this-is-a-test',
+        fs.sluggify_title(' ___ !~!!$%^ This iS a tEsT ++ ?? ')
+      )
+    end
+  )
 end)
 
-describe('Utils.sluggify_tag()', function()
+describe('fs.sluggify_keyword()', function()
   it('downcases, de-punctuates, and removes delimiters from string', function()
-    eq('thisisatest', Utils.sluggify_tag(' ___ !~!!$%^ This iS a tEsT ++ ?? '))
+    eq('thisisatest', fs.sluggify_keyword(' ___ !~!!$%^ This iS a tEsT ++ ?? '))
   end)
 end)
 
-describe('Utils.build_file_stem()', function()
+describe('fs.build_file_stem()', function()
   it('handles no-space title and 1 tag', function()
-    local ts = Utils.make_timestamp()
-    local stem = Utils.build_file_stem(ts, 'foo', 'bar')
+    local ts = fs.make_timestamp()
+    local stem = fs.build_file_stem(ts, 'foo', 'bar')
 
     assert(type(stem) == 'string', 'result should be a string')
     assert(stem:find(ts), 'should contain timestamp')
@@ -92,8 +102,8 @@ describe('Utils.build_file_stem()', function()
   end)
 
   it('handles title with spaces, multiple tags', function()
-    local ts = Utils.make_timestamp()
-    local stem = Utils.build_file_stem(ts, 'foo bar', 'bar1,bar2')
+    local ts = fs.make_timestamp()
+    local stem = fs.build_file_stem(ts, 'foo bar', 'bar1,bar2')
 
     assert(type(stem) == 'string', 'result should be a string')
     assert(stem:find(ts), 'should contain timestamp')
@@ -116,8 +126,8 @@ describe('Utils.build_file_stem()', function()
   end)
 
   it('handles args with mismatched dash/underscores', function()
-    local ts = Utils.make_timestamp()
-    local stem = Utils.build_file_stem(ts, 'foo_bar', 'bar_1,bar2')
+    local ts = fs.make_timestamp()
+    local stem = fs.build_file_stem(ts, 'foo_bar', 'bar_1,bar2')
 
     assert(type(stem) == 'string', 'result should be a string')
     assert(stem:find(ts), 'should contain timestamp')
@@ -140,8 +150,8 @@ describe('Utils.build_file_stem()', function()
   end)
 
   it('handles args with breaking dash/underscore patterns', function()
-    local ts = Utils.make_timestamp()
-    local stem = Utils.build_file_stem(ts, '--foo__bar__', '__bar_1,bar__2')
+    local ts = fs.make_timestamp()
+    local stem = fs.build_file_stem(ts, '--foo__bar__', '__bar_1,bar__2')
 
     assert(type(stem) == 'string', 'result should be a string')
     assert(stem:find(ts), 'should contain timestamp')
